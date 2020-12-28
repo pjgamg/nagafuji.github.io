4908e3110b4305e1b1b7a7ed2e2998879833c912
pjgamg <pjgamg@hotmail.co.jp>
2020-12-28 15:42:06 +0900

    vue practice

--- js/my_vue.js
+++ js/my_vue.js
@@ -6,17 +6,17 @@
         data: {
             // name: 'nagafuji'
             newItem: "",
-            // todos: [{
-            //     title: 'task 1',
-            //     isDone: false
-            // }, {
-            //     title: 'task 2',
-            //     isDone: false
-            // }, {
-            //     title: 'task 3',
-            //     isDone: true
-            // }]
-            todos: []
+            todos: [{
+                title: 'task 1',
+                isDone: false
+            }, {
+                title: 'task 2',
+                isDone: false
+            }, {
+                title: 'task 3',
+                isDone: true
+            }]
+            // todos: []
         },
         // methods: {
         //     addItem: function (e) {
@@ -36,6 +36,26 @@
                 if (confirm('are you sure?')) {
                     this.todos.splice(index, 1);
                 }
+            },
+            purge: function () {
+                if (!confirm('delete finished?')) {
+                    return;
+                }
+                // this.todos = this.todos.filter(function (todo) {
+                //     return !todo.isDone;
+                // });
+                this.todos = this.remaining;
+            },
+        },
+        computed: {
+            remaining: function () {
+                // var items = this.todos.filter(function (todo) {
+                //     return !todo.isDone;
+                // });
+                // return items.length;
+                return this.todos.filter(function (todo) {
+                    return !todo.isDone;
+                });
             }
         }
     });
